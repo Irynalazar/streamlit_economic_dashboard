8727d9e79dd113eda6b60d4abec8ae06140fb0f2
Irynalazar <42173216+Irynalazar@users.noreply.github.com>
2025-05-15 07:38:02 +0300

    app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1pPbxylj233p_KCp0b8wXPNG5yDEg2keg
 """
 
-# Завантажимо Streamlit
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
